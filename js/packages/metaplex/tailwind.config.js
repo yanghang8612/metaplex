61501ded301e8642425822aa169d0569820e1bfe
Ryan Shea <rmshea@users.noreply.github.com>
2021-01-04 10:38:29 -0700

    Add fonts, attempt to implement them

--- js/packages/metaplex/tailwind.config.js
+++ js/packages/metaplex/tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   purge: ["./src/**/**/*.{js,jsx,ts,tsx}"], // purge from all folders
   darkMode: false, // or 'media' or 'class'
+  fontFamily: {
+    'serif': ['Maiola'],
+    'sans': ["Termina Demi"]
+  },
   theme: {
     extend: {},
   },
