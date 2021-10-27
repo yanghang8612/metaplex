6f2b578e56ad73a2202625b2a0e45720e1e56947
DR497 <47689875+dr497@users.noreply.github.com>
2021-10-27 11:12:05 +0800

    add bidder pot serialization

--- rust/auction/program/src/processor/create_bidder_pot.rs
+++ rust/auction/program/src/processor/create_bidder_pot.rs
@@ -1,15 +1,18 @@
 use crate::{
+    errors::AuctionError,
     processor::BidderPot,
     utils::{
         assert_account_key, assert_derivation, assert_owned_by, assert_signer,
-        create_or_allocate_account_raw,
+        create_or_allocate_account_raw, assert_initialized
     },
     PREFIX,
 };
 
+use borsh::BorshSerialize;
 use {
     solana_program::{
         account_info::{next_account_info, AccountInfo},
+        borsh::try_from_slice_unchecked,
         entrypoint::ProgramResult,
         msg,
         program_error::ProgramError,
@@ -18,6 +21,7 @@ use {
         system_instruction::create_account,
         sysvar::{rent, Sysvar},
     },
+    spl_token::state::Account,
     std::mem,
 };
 
@@ -25,6 +29,7 @@ struct Accounts<'a, 'b: 'a> {
     auction: &'a AccountInfo<'b>,
     bidder_pot: &'a AccountInfo<'b>,
     bidder: &'a AccountInfo<'b>,
+    bidder_pot_token: &'a AccountInfo<'b>,
     transfer_authority: &'a AccountInfo<'b>,
     payer: &'a AccountInfo<'b>,
     rent: &'a AccountInfo<'b>,
@@ -40,6 +45,7 @@ fn parse_account<'a, 'b: 'a>(
     let accounts = Accounts {
         auction: next_account_info(account_iter)?,
         bidder_pot: next_account_info(account_iter)?,
+        bidder_pot_token: next_account_info(account_iter)?,
         bidder: next_account_info(account_iter)?,
         transfer_authority: next_account_info(account_iter)?,
         payer: next_account_info(account_iter)?,
@@ -53,6 +59,7 @@ fn parse_account<'a, 'b: 'a>(
     assert_signer(accounts.transfer_authority)?;
     assert_account_key(accounts.rent, &solana_program::sysvar::rent::ID)?;
     assert_account_key(accounts.system, &solana_program::system_program::ID)?;
+    assert_owned_by(accounts.bidder_pot_token, &spl_token::id())?;
     return Ok(accounts);
 }
 
@@ -62,6 +69,11 @@ pub fn create_bidder_pot<'a, 'b: 'a>(
 ) -> ProgramResult {
     msg!("+ Processing CreateBidderPot");
     let accounts = parse_account(program_id, accounts)?;
+
+    let actual_account: Account = assert_initialized(accounts.bidder_pot_token)?;
+    if actual_account.owner != *accounts.auction.key {
+        return Err(AuctionError::BidderPotTokenAccountOwnerMismatch.into());
+    }
 
     let pot_bump = assert_derivation(
         program_id,
@@ -92,6 +104,11 @@ pub fn create_bidder_pot<'a, 'b: 'a>(
             mem::size_of::<BidderPot>(),
             bump_authority_seeds,
         )?;
+        let mut pot: BidderPot = try_from_slice_unchecked(&accounts.bidder_pot.data.borrow_mut())?;
+        pot.bidder_pot = *accounts.bidder_pot_token.key;
+        pot.bidder_act = *accounts.bidder.key;
+        pot.auction_act = *accounts.auction.key;
+        pot.serialize(&mut *accounts.bidder_pot.data.borrow_mut())?;
     } else {
         msg!("Bidder pot already exists");
         return Err(ProgramError::AccountAlreadyInitialized);
